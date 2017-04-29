4f9e8bb30bb24cdb30989e691908b51c77c759ae
Wanderson O. da Silva <wanderson.olivs@gmail.com>
2017-04-29 15:14:52 -0300

    docs(READmyREADME): new documentations of the project; fix(file reading and writing): closes the file when it doesn't exist and show the error and line if occur any problema during the reading and fix the file output name created bt run method; test(TestJarvis): improved testing; chore(marsrover): now, it asks for the file name.

--- classes/Jarvis.rb
+++ classes/Jarvis.rb
@@ -7,6 +7,13 @@ class Jarvis
   attr_accessor :plateau
   attr_accessor :inputFileLines
   attr_reader :outputFileLines
+  @validRoverOrientations
+  @validRoverInstructions
+
+  def initialize
+    @validRoverOrientations = ["N", "S", "W", "E"]
+    @validRoverInstructions = ["M", "R", "L"]
+  end
 
   public
   def readInput(path)
@@ -21,13 +28,21 @@ class Jarvis
       end
     else
       puts "The mencioned input file doesn't exist!\nCome on, you're a ninja. Please, check it out."
+      exit
     end
+
+    #validate the inputted file
+    if !isAValidFileInput?
+      @inputFileLines = nil
+      printExample()
+    end
+
   end
 
   public
   def executeCommands
     if !@inputFileLines || @inputFileLines.length == 0
-      puts "No file was imported."
+      puts "No file have been imported."
       return
     end
 
@@ -68,7 +83,7 @@ class Jarvis
 
       #otherwise, it's the instructions line
       else
-        #separate the instructions inside an array eliminate the \n at the end
+        #separate the instructions inside an array and eliminate the \n at the end
         instructions = @inputFileLines[i].upcase.chomp.split(//)
 
         for x in (0...instructions.length)
@@ -91,6 +106,41 @@ class Jarvis
       end
     end
   end
+
+  #write the output file and puts it on the screen
+  public
+  def writeOutput(path)
+     if !@outputFileLines || @outputFileLines.length == 0
+       "Nothing to export."
+       return
+     end
+
+     puts "\nFinal rovers position(s):"
+     rover = 0
+
+     outputFile = File.open(path, "w")
+     for line in @outputFileLines
+       outputFile << line + "\n"
+       puts "------Rover " + (rover += 1).to_s() + "------"
+       puts line + "\n"
+     end
+     outputFile.close
+   end
+
+   #the method run can be used to make Jarvis execute all at once
+
+  public
+  def run(path)
+     readInput(path)
+     if !@inputFileLines || @inputFileLines.length == 0
+       puts "\nNo file has been imported."
+     else
+       executeCommands
+       writeOutput(path[0, path.length - 4] + "_OUTPUT.txt")
+     end
+   end
+
+   #based on plateau area, determines if a rover can move forward or not
 
   private
   def canMoveForward?(rover)
@@ -119,32 +169,77 @@ class Jarvis
     return false
   end
 
- #write the output file and puts it on the screen
- public
-  def writeOutput(path)
-    if !@outputFileLines || @outputFileLines.legth == 0
-      "Nothing to export."
-      return
+  #this method has all the rules for a valid file to be inputted
+  private
+  def isAValidFileInput?
+    if @inputFileLines.length < 2
+      return false
     end
 
-    outputFile = File.open(path, "w")
+    for l in (0...@inputFileLines.length)
+      #check if is the first line
+      if l == 0
+        plateau = @inputFileLines[0].split(' ')
 
-    for line in @outputFileLines
-      outputFile << line + "\n"
-      #puts line + "\n"
+          #check the amount of elements
+          if plateau.size != 2
+            puts "Invalid line: " + (l + 1).to_s() + " - Invalid plateau size."
+            return false
+          end
+
+          #check if all elements are numeric
+          begin
+            Integer(plateau[0])
+            Integer(plateau[1])
+          rescue
+            puts "Invalid line: " + (l + 1).to_s() + " - Plateau coordinates are not numbers."
+            return false
+          end
+
+          #check if is a rover's initial position line
+      elsif l%2 == 1
+        initialPosition = @inputFileLines[l].split(" ")
+
+        if initialPosition.length != 3
+          puts "Invalid line: " + (l + 1).to_s() + " - The line has more or less"\
+           + " than 3 elements that must be separated by blank spaces."
+          return false
+
+        elsif !@validRoverOrientations.include? initialPosition[2]
+          puts "Invalid line: " + (l + 1).to_s() + " - The rovers orientation is invalid."
+          return false
+
+        else
+          begin
+            Integer(initialPosition[0])
+            Integer(initialPosition[1])
+          rescue
+            puts "Invalid line: " + (l + 1).to_s() + " - The first two elements of the line are not valid numbers!"
+            return false
+          end
+        end
+
+      else
+        instructions = @inputFileLines[l].chomp.split(//)
+        for i in instructions
+          if !@validRoverInstructions.include? i.upcase()
+            puts "Invalid line: " + (l + 1).to_s() + " - There must be only valid letters for rover instructions."
+            return false
+          end
+        end
+      end
     end
-    outputFile.close
+    return true;
   end
 
-  #the method run can be used to make Jarvis execute all at once
-  public
-  def run(path)
-    readInput(path)
-    if !@inputFileLines || @inputFileLines.length == 0
-      puts "No file was imported."
-    else
-      executeCommands
-      writeOutput(path[0, path.length - 5] + "_OUTPUT.txt")
+  private
+  def printExample
+    puts "\nInvalid file! Please, use an input with at least two lines following the example below:"
+    puts "\n5 5"
+    puts "1 2 N"
+    puts "MMMLRMRLMRL"
+    puts "3 5 S"
+    puts "LRRRMRLMRLMMM"
   end
 
 end
--- marsrover.rb
+++ marsrover.rb
@@ -1,16 +1,15 @@
 require_relative 'classes/Jarvis'
 
-jarvis = Jarvis.new
-
-puts "Qual o nome do arquivo para importação? "
-#fileName = gets.chomp
-
-jarvis.readInput("input" + ".txt")
-jarvis.executeCommands
-jarvis.writeOutput("input" + "_OUTPUT.txt")
-
-
 =begin
   It's necessary for the file to be inside the "io" folder in project root folder
   so it can be read
 =end
+puts "Qual o nome do arquivo para importação? "
+fileName = gets.chomp
+
+jarvis = Jarvis.new
+jarvis.run("io/" + fileName + ".txt")
+
+#jarvis.readInput("io/" + "input" + ".txt")
+#jarvis.executeCommands
+#puts jarvis.writeOutput("input" + "_OUTPUT.txt")
